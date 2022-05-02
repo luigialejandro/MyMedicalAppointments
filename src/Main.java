0407466fdaf8f5756e377bf5e649be0dcc9d212c
luigialejandro <50028217+luigialejandro@users.noreply.github.com>
2022-05-02 16:28:49 -0500

    So far I have learned and applied new fundamental concepts in Java such as constructor overloadig and encapsulation. Using the reserved word static enables the possibility of calling any method or attribute in any other class as long as we also use the appropriate access modifiers

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
+import static ui.UIMenu.*;
+
 public class Main {
     public static void main(String[] args){
 
-        Doctor myDoctor = new Doctor();
-        myDoctor.name = "Alejandro Gómez";
-        myDoctor.showName();
+        //showMenu();
 
+        Doctor myDoctor = new Doctor("Anahí Salgado", "Pediatria");
+        System.out.println(myDoctor.name);
+        System.out.println(myDoctor.speciality);
+
+        Patient patient = new Patient("Alejandra", "alejandra@mail.com");
     }
 }
