259225ea777a10a0777c35f5484bef9006a8af9e
luigialejandro <50028217+luigialejandro@users.noreply.github.com>
2022-05-02 20:41:31 -0500

    Inside of Doctor Class I've nested another class, namely a static nested Class called AvailableAppointment which has its own attributes, it own builder and its own getters and setters. At the same level of doctor I've created a method called addAvailableAppointment which uses my nested class in order to create an ArrayList of instances of the nested class so that every time this method is called then a new appointment will be created

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+
 import static ui.UIMenu.*;
 
 public class Main {
@@ -6,14 +8,25 @@ public class Main {
         //showMenu();
 
         Doctor myDoctor = new Doctor("Anahí Salgado", "Pediatria");
-        System.out.println(myDoctor.name);
-        System.out.println(myDoctor.speciality);
 
-        Patient patient = new Patient("Alejandra", "alejandra@mail.com");
+        myDoctor.addAvailableAppointment(new Date(), "4 PM");
+        myDoctor.addAvailableAppointment(new Date(), "10 AM");
+        myDoctor.addAvailableAppointment(new Date(), "1 PM");
+
+        System.out.println(myDoctor.getAvailableAppointments());
+
+        for (Doctor.AvailableAppointment aA: myDoctor.getAvailableAppointments()){
+            System.out.println(aA.getDate()+""+aA.getTime());
+        }
+
+        //System.out.println(myDoctor.name);
+        //System.out.println(myDoctor.speciality);
+
+        /*Patient patient = new Patient("Alejandra", "alejandra@mail.com");
         patient.setWeight(54.6);
         System.out.println(patient.getWeight());
 
         patient.setPhoneNumber("123456789");
-        System.out.println(patient.getPhoneNumber());
+        System.out.println(patient.getPhoneNumber());*/
     }
 }
